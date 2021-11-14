04af2efab71d809f42bc2d083bd5a72f760abf79
LLHackzul <alejandromkup24@gmail.com>
2021-11-13 20:02:15 -0600

    Added model, config file and json with data

--- carlist/models.py
+++ carlist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    line = models.TextField()
+    color = models.TextField()
+    transmition = models.TextField()
+    model = models.TextField()
+    category = models.ForeignKey(
+        Category, related_name="cars", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
